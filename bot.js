e2dce51c4c70b88ab2935b9cb47b3d7c49449bfe
iiklay <40916077+iiklay@users.noreply.github.com>
2018-09-12 16:30:49 +0200

    Update bot.js

--- bot.js
+++ bot.js
@@ -36,19 +36,7 @@
     })
     })
     }
-    });
-  client.on('message', msg => {
-  if(msg.content === ('باك')
-  msg.reply('ولكممممم')
-});
-  client.on('message', msg => {
-  if(msg.content === 'برب')
-  msg.reply('تيت')
-});
-        client.on('message', msg => {
-  if(msg.content === 'السلام عليكم')
-  msg.reply('وعليكم السلام')
-});
+
 
       const Discord = require('discord.js');
 const client = new Discord.Client();
