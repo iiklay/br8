1075af5823d40861594824ed500e28f366a12daa
iiklay <40916077+iiklay@users.noreply.github.com>
2018-09-13 11:24:27 +0200

    Update bot.js

--- bot.js
+++ bot.js
@@ -392,7 +392,7 @@ client.on('message',async message => {
 
   let args = message.content.split(" ").slice(1);
 
-  if (command == "ban$") {
+  if (command == "ban") {
                if(!message.channel.guild) return message.reply('** This command only for servers**');
          
   if(!message.guild.member(message.author).hasPermission("BAN_MEMBERS")) return message.reply("**انت لا تملك الصلاحيات المطلوبه**");
